baf115fc5d0684807b5ad3bfb62c03ebf6965e7f
Duy2763 <tduy2706@gmail.com>
2024-10-11 23:37:49 +0700

    done

---
+++ src/main/java/dao/DAONhaCungCapInterface.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.util.List;
+
+import entities.NhaCungCap;
+
+public interface DAONhaCungCapInterface {
+	public boolean addNhaCungCap(NhaCungCap ncc);
+	public List<NhaCungCap> getAllNCC();
+	public NhaCungCap getNCCById(int id);
+}
